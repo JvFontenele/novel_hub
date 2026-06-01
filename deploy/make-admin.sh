7fe5ca3e15545b03baee53e0c2fd0f14587a5577
joaovnf <jvfont80@gmail.com>
2026-06-01 12:08:35 -0300

    fix: corrige sintaxe SQL no make-admin.sh

--- deploy/make-admin.sh
+++ deploy/make-admin.sh
@@ -12,8 +12,7 @@ fi
 
 UPDATED=$(docker compose -f docker-compose.prod.yml exec -T postgres \
   psql -U novel_hub -d novel_hub -tAc \
-  "UPDATE users SET role='admin' WHERE email = :'email' RETURNING email;" \
-  -v "email=$EMAIL")
+  "UPDATE users SET role='admin' WHERE email = '$EMAIL' RETURNING email;")
 
 if [[ -z "$UPDATED" ]]; then
   echo "No user found with email: $EMAIL"
